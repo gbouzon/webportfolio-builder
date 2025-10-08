95e46e77d7489fd5697fbc7692780adba62f6243
NoahPlourdeM <NoahPlourdeM@hotmail.com>
2025-10-07 20:49:56 -0400

    WIP

--- portfolio-builder/src/components/customelements/Header.vue
+++ portfolio-builder/src/components/customelements/Header.vue
@@ -2,14 +2,18 @@
 const emit = defineEmits(["AddComponent"])
 </script>
 <template>
-    <v-sheet border="md" class="pa-6 text-white mx-auto" color="#141518">
+    <v-sheet rounded border="sm" class="text-white mx-auto" color="#141518">
         <v-container>
-            <v-row align="start" style="height: 150px;" no-gutters>
-                <v-col cols="2">
-    
+            <v-row align="start" no-gutters>
+                <v-col cols="2" no-gutters>
+                    <v-icon icon="fa:fas fa-user">
+                    </v-icon>
                 </v-col>
-                <v-col cols="10">
-    
+                <v-col cols="1" no-gutters>
+                    <v-divider vertical></v-divider>
+                </v-col>
+                <v-col cols="9" no-gutters>
+                    <p>Header</p>
                 </v-col>
             </v-row>
         </v-container>
--- portfolio-builder/src/plugins/vuetify.js
+++ portfolio-builder/src/plugins/vuetify.js
@@ -7,13 +7,22 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases,fa } from 'vuetify/iconsets/fa'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'system',
   },
+  icons:{
+    defaultSet:"fa",
+    aliases,
+    sets:{
+      fa
+    }
+  }
 })
